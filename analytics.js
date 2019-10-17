cbf700e2d6d4d47f1d84d60a172cd4e07c576ae1
Tyler Tolson <tylertolson1@gmail.com>
2019-10-17 17:18:30 -0400

    Changed the speed of particles.js

--- analytics.js
+++ analytics.js
@@ -1,6 +1,4 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() {
-	dataLayer.push(arguments);
-}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'UA-62747905-7');
