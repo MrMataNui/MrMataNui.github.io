b59995cf58b06879a8dd474a207701a0ca425725
Tyler Tolson <tylertolson1@gmail.com>
2019-11-11 10:29:38 -0500

    added a mobile navbar

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function formatDate(format, date) {
-	const YYYY = date.year;
-	const YY = date.year.split('20')[1];
-	const MM = date.month;
-	const DD = date.day;
-	switch (format) {
-		case 'YYYY.MM.DD': return `${YYYY}.${MM}.${DD}`;
-		case 'MM/DD/YYYY': return `${MM}/${DD}/${YYYY}`;
-		case 'DD.MM.YYYY': return `${DD}.${MM}.${YYYY}`;
-		case 'MM/DD/YY': return `${MM}/${DD}/${YY}`;
-		case 'DD.MM.YY': return `${DD}.${MM}.${YY}`;
-	}
-}
 $(() => {
-	$('#martian date').text(formatDate('YYYY.MM.DD', {
-		year: '2017', month: '08', day: '23'
-	}));
-	$('#claymation date').text(formatDate('YYYY.MM.DD', {
-		year: '2019', month: '01', day: '10'
-	}));
+	$('#show-nav').click(() => {
+		if ($('#myLinks').css('display') === 'block') {
+			$('#myLinks').css({ 'display': 'none' });
+		} else {
+			$('#myLinks').css({ 'display': 'block' });
+		}
+  });
+
+	const martianDate = { year: '2017', month: '08', day: '23' };
+	const claymationDate = { year: '2019', month: '01', day: '10' };
+
+	const formatType = 'YYYY.MM.DD';
+	const formatDate = (date) => formatType
+		.replace(/YYYY/, date.year)
+		.replace(/YY/, date.year.split('20')[1])
+		.replace(/MM/, date.month)
+		.replace(/DD/, date.day);
+
+	$('#martian date').text(formatDate(martianDate));
+	$('#claymation date').text(formatDate(claymationDate));
 });
